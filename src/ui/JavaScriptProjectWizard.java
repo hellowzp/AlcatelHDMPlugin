fdb33c1eec2f4d0e16e421e86c96ea885925384a
wang <zpkx.wang@gmail.com>
2015-03-14 15:31:42 +0100

    "lib update"

--- src/ui/JavaScriptProjectWizard.java
+++ src/ui/JavaScriptProjectWizard.java
@@ -1,16 +1,10 @@
 package ui;
 
 import java.io.ByteArrayInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.PrintWriter;
 import java.lang.reflect.InvocationTargetException;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
